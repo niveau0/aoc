4b6e4b1a9ae7e8a0afa9d5224c3c7430a65d8ebe
niveau0 <plingplong@t-online.de>
2020-12-04 16:15:26 +0100

    2020 day 4 part 1

---
+++ 2020/4/part1.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+VALID=0
+INVALID=0
+
+REQ=(byr iyr eyr hgt hcl ecl pid)
+
+while read LINE; do
+  if [[ -z $LINE ]]; then
+    echo PW ${PW[@]}
+
+    FOUNDREQ=0
+    for i in ${PW[@]}; do
+        for j in ${REQ[@]}; do
+        if [[ $i == $j* ]]; then
+            FOUNDREQ=$(($FOUNDREQ+1))
+            break   
+        fi
+        done
+    done
+
+    if [ $FOUNDREQ == ${#REQ[@]@} ]
+    then
+        echo VALID ${PW[@]}
+        VALID=$(($VALID+1))
+    else
+        INVALID=$(($INVALID+1))
+    fi
+
+    PW=()
+
+    continue
+  fi
+
+  PW=(${PW[@]} ${LINE[@]})
+
+done <puzzle.txt
+
+echo "Valid $VALID"
+echo "Invalid $INVALID"
